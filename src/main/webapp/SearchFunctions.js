a602a1cc556081bdaedeb0bf43da36bd54411aa9
Ari Fainchtein <ari@digitalgeppetto.com>
2018-08-14 08:54:14 +1000

    developing search component

--- src/main/webapp/SearchFunctions.js
+++ src/main/webapp/SearchFunctions.js
@@ -21,7 +21,7 @@ class SearchFunctions{
         $('#WaitingText').html("Updating...");
        $('#WaitingWheel').show();
     
-       generateAllGraphs();
+       this.generateAllGraphs();
     
        
         
