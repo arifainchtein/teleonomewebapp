1cf3cab2ed4c7e167cf037e25872fb6cf0244088
Ari Fainchtein <ari@digitalgeppetto.com>
2018-10-10 08:31:28 +1100

    fixing memory info

--- src/main/webapp/templates/NewRememberedDeneWordWizard.js
+++ src/main/webapp/templates/NewRememberedDeneWordWizard.js
@@ -8,6 +8,32 @@ class NewRememberedDeneWordWizard{
         
         var panelHTML = "";
         
+          
+        panelHTML += "              <div class=\"col-lg-12\">";
+        panelHTML += "                  <div class=\"bs-component\">";
+        panelHTML += "                      <div class=\"panel panel-default\">";
+        panelHTML += "                          <div class=\"panel-heading\"><h4><span id=\"ModalHeadingText\"></span></h4></div>";
+        panelHTML += "                          <div class=\"panel-body text-center\">";
+        panelHTML += "                              <div class=\"well\" id=\"NewRememberedWordIdentity\" class=\"row\"></div>";
+        panelHTML += "                              <div id=\"NewRememberedWordOrganismView\" class=\"row\"></div>";
+        panelHTML += "                              <div id=\"NewRememberedWordNucleusView\" class=\"row\"></div>";
+        panelHTML += "                              <div id=\"NewRememberedWordDeneChainView\" class=\"row\"></div>";
+        panelHTML += "                              <div id=\"NewRememberedWordDeneView\" class=\"row\"></div>";
+        panelHTML += "                              <div id=\"NewRememberedWordDeneWordView\" class=\"row\"></div>";
+        panelHTML += "                          </div>";
+        panelHTML += "                      </div>"; //panel panel-default           
+        panelHTML += "                  </div>";   //bs-component  
+        panelHTML += "              </div>"; // col-lg-12
+        
+        return panelHTML;
+    }
+
+
+
+    process1(){
+        
+        var panelHTML = "";
+        
         panelHTML += "<div class=\"modal fade bs-example-modal-lg\" tabindex=\"-1\" id=\"NewRememberedDeneWordWizard\" role=\"dialog\" aria-labelledby=\"ModalHeadingText\" aria-hidden=\"true\">";
         panelHTML += "   <div class=\"modal-dialog modal-lg\">";
         panelHTML += "       <div class=\"modal-content\">";
@@ -35,7 +61,7 @@ class NewRememberedDeneWordWizard{
 
 
         
-        panelHTML += "                              <div class=\"well\" id=\"NewRememberedWordIdentity\" class=\"row\"></div>";
+        panelHTML += "                              <div class=\"well\" id=\"NewRememberedWordIdentity\" ></div>";
         panelHTML += "                              <div id=\"NewRememberedWordOrganismView\" class=\"row\"></div>";
         panelHTML += "                              <div id=\"NewRememberedWordNucleusView\" class=\"row\"></div>";
         panelHTML += "                              <div id=\"NewRememberedWordDeneChainView\" class=\"row\"></div>";
--- src/main/webapp/templates/SearchPanel.js
+++ src/main/webapp/templates/SearchPanel.js
@@ -41,7 +41,32 @@ class SearchPanel{
         panelHTML += "				    </div>";
         panelHTML += "                </div>";
         
-        
+		//
+		// the panel for the new remembered
+		//
+		panelHTML += "          <div id=\"NewRememberedWordPanel\" class=\"row collapse \">";
+		panelHTML += "              <div class=\"col-lg-12\">";
+        panelHTML += "                  <div class=\"bs-component\">";
+        panelHTML += "                      <div class=\"panel panel-default\">";
+        panelHTML += "                          <div class=\"panel-heading\"><h4><span id=\"ModalHeadingText\"></span></h4></div>";
+        panelHTML += "                          <div class=\"panel-body text-center\">";
+        panelHTML += "                              <div class=\"well\" id=\"NewRememberedWordIdentity\" class=\"row\"></div>";
+        panelHTML += "                              <div id=\"NewRememberedWordOrganismView\" class=\"row\"></div>";
+        panelHTML += "                              <div id=\"NewRememberedWordNucleusView\" class=\"row\"></div>";
+        panelHTML += "                              <div id=\"NewRememberedWordDeneChainView\" class=\"row\"></div>";
+        panelHTML += "                              <div id=\"NewRememberedWordDeneView\" class=\"row\"></div>";
+        panelHTML += "                              <div id=\"NewRememberedWordDeneWordView\" class=\"row\"></div>";
+        panelHTML += "                          </div>";
+        panelHTML += "                      </div>"; //panel panel-default           
+        panelHTML += "                  </div>";   //bs-component  
+		panelHTML += "              </div>"; // col-lg-12
+		panelHTML += "          </div>"; // row
+		//
+		// end the panel for the new remembered
+		//
+
+
+
         var screensize = document.documentElement.clientWidth;
         if(screensize>500){
 	        panelHTML += "                <div id=\"SearchConfigurator\"  class=\"SearchConfigurator row\" style=\"display:none\">";
