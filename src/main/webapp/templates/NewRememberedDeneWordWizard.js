be37dda4aaea277cddfb7d89264b212933ae0661
Ari Fainchtein <ari@digitalgeppetto.com>
2018-10-09 17:12:43 +1100

    fixing memory info

--- src/main/webapp/templates/NewRememberedDeneWordWizard.js
+++ src/main/webapp/templates/NewRememberedDeneWordWizard.js
@@ -13,14 +13,15 @@ class NewRememberedDeneWordWizard{
         panelHTML = "       <div class=\"modal-content\">";
         panelHTML = "           <div class=\"modal-header\">";
         panelHTML = "               <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>";
-        panelHTML = "               <h4 class=\"modal-title\" id=\"myModalLabel\">Start Remembering</h4>";
+        panelHTML = "               <h4 class=\"modal-title\" id=\"NewRememberedDeneWordWizard\">Start Remembering</h4>";
         panelHTML = "           </div>";
 
         panelHTML = "           <div class=\"col-lg-12\">";
         panelHTML += "              <div class=\"bs-component\">";
         panelHTML += "                  <div class=\"panel panel-default\">";
-        panelHTML += "                      <div class=\"panel-heading\"><h4>Select the Teleonome</h4></div>";
+        panelHTML += "                      <div class=\"panel-heading\"><h4><span id=\"ModalHeadingText\"></span></h4></div>";
         panelHTML += "                      <div class=\"panel-body text-center\">";
+        panelHTML += "                          <div class=\"well\" id=\"NewRememberedWordIdentity\" class=\"row\"></div>";
         panelHTML += "                          <div id=\"NewRememberedWordOrganismView\" class=\"row\"></div>";
         panelHTML += "                          <div id=\"NewRememberedWordNucleusView\" class=\"row\"></div>";
         panelHTML += "                          <div id=\"NewRememberedWordDeneChainView\" class=\"row\"></div>";
