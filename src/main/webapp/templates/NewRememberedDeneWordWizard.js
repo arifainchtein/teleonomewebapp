58e5f4f708d3f003a1e1f1aaf1618de64384325a
Ari Fainchtein <ari@digitalgeppetto.com>
2018-10-10 07:13:14 +1100

    fixing memory info

--- src/main/webapp/templates/NewRememberedDeneWordWizard.js
+++ src/main/webapp/templates/NewRememberedDeneWordWizard.js
@@ -8,30 +8,44 @@ class NewRememberedDeneWordWizard{
         
         var panelHTML = "";
         
-        panelHTML += "<div class=\"modal fade bs-example-modal-lg\" tabindex=\"-1\" id=\"NewRememberedDeneWordWizard\" role=\"dialog\" aria-labelledby=\"NewRememberedDeneWordWizardLabel\" aria-hidden=\"true\">";
+        panelHTML += "<div class=\"modal fade bs-example-modal-lg\" tabindex=\"-1\" id=\"NewRememberedDeneWordWizard\" role=\"dialog\" aria-labelledby=\"ModalHeadingText\" aria-hidden=\"true\">";
         panelHTML += "   <div class=\"modal-dialog modal-lg\">";
         panelHTML += "       <div class=\"modal-content\">";
         panelHTML += "           <div class=\"modal-header\">";
         panelHTML += "               <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>";
-        panelHTML += "               <h4 class=\"modal-title\" id=\"NewRememberedDeneWordWizardLabel\">Start Remembering</h4>";
+        panelHTML += "               <h4 class=\"modal-title\" id=\"ModalHeadingText\">Start Remembering</h4>";
         panelHTML += "           </div>";
         panelHTML += "           <div class=\"modal-body\">";
-        panelHTML += "              <div class=\"col-lg-12\">";
-        panelHTML += "                  <div class=\"bs-component\">";
-        panelHTML += "                      <div class=\"panel panel-default\">";
+        
+        // panelHTML += "              <div class=\"col-lg-12\">";
+        // panelHTML += "                  <div class=\"bs-component\">";
+        // panelHTML += "                      <div class=\"panel panel-default\">";
+        // panelHTML += "                          <div class=\"panel-heading\"><h4><span id=\"ModalHeadingText\"></span></h4></div>";
+        // panelHTML += "                          <div class=\"panel-body text-center\">";
+        // panelHTML += "                              <div class=\"well\" id=\"NewRememberedWordIdentity\" class=\"row\"></div>";
+        // panelHTML += "                              <div id=\"NewRememberedWordOrganismView\" class=\"row\"></div>";
+        // panelHTML += "                              <div id=\"NewRememberedWordNucleusView\" class=\"row\"></div>";
+        // panelHTML += "                              <div id=\"NewRememberedWordDeneChainView\" class=\"row\"></div>";
+        // panelHTML += "                              <div id=\"NewRememberedWordDeneView\" class=\"row\"></div>";
+        // panelHTML += "                              <div id=\"NewRememberedWordDeneWordView\" class=\"row\"></div>";
+        // panelHTML += "                          </div>";
+        // panelHTML += "                      </div>"; //panel panel-default           
+        // panelHTML += "                  </div>";   //bs-component  
+        // panelHTML += "              </div>"; // col-lg-12
 
-        panelHTML += "                          <div class=\"panel-heading\"><h4><span id=\"ModalHeadingText\"></span></h4></div>";
-        panelHTML += "                          <div class=\"panel-body text-center\">";
+
+        
         panelHTML += "                              <div class=\"well\" id=\"NewRememberedWordIdentity\" class=\"row\"></div>";
         panelHTML += "                              <div id=\"NewRememberedWordOrganismView\" class=\"row\"></div>";
         panelHTML += "                              <div id=\"NewRememberedWordNucleusView\" class=\"row\"></div>";
         panelHTML += "                              <div id=\"NewRememberedWordDeneChainView\" class=\"row\"></div>";
         panelHTML += "                              <div id=\"NewRememberedWordDeneView\" class=\"row\"></div>";
         panelHTML += "                              <div id=\"NewRememberedWordDeneWordView\" class=\"row\"></div>";
-        panelHTML += "                          </div>";
-        panelHTML += "                      </div>"; //panel panel-default           
-        panelHTML += "                  </div>";   //bs-component  
-        panelHTML += "              </div>"; // col-lg-12
+        
+       
+
+
+
         panelHTML += "          </div>"; //modal-body
         panelHTML += "        </div>"; //modal-content
         panelHTML += "    </div>";//modal-dialog
