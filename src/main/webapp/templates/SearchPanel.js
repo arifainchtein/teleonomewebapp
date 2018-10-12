a8b2ec1560e1a4f7856482d26812c2d3eac0ce07
Ari Fainchtein <ari@digitalgeppetto.com>
2018-10-13 06:39:04 +1100

    fixing memory info

--- src/main/webapp/templates/SearchPanel.js
+++ src/main/webapp/templates/SearchPanel.js
@@ -13,14 +13,17 @@ class SearchPanel{
         panelHTML += "                <div class=\"row-fluid\">";
         panelHTML += "                    <div id=\"TopButtons\" class=\"pull-left TopButtons col-xs-8\">";
         panelHTML += "                        <button class=\"btn btn-default\" type=\"button\" id=\"NewChart\" type=\"submit\" value=\"Submit\"><em class=\"glyphicon glyphicon-plus\"></em>New</button>";
-        panelHTML += "                        <button class=\"btn btn-default\" type=\"button\" id=\"ClearCharts\" type=\"submit\" value=\"Submit\"><em class=\"glyphicon glyphicon-remove\"></em>Clear</button>";
+        panelHTML += "                        <button class=\"btn btn-default\" type=\"button\" id=\"ClearCharts\" type=\"submit\" value=\"Submit\"   style=\" margin-right:30px;\"><em class=\"glyphicon glyphicon-remove\"></em>Clear</button>";
 
 		if(organismInfoJsonData != undefined){
 			panelHTML += "                        <button class=\"btn btn-default\"  data-toggle=\"modal\" data-target=\"#NewRememberedDeneWordWizard\" type=\"button\" id=\"NewRemember\" type=\"submit\" value=\"Submit\" ><em class=\"glyphicon glyphicon-record\"></em>Remember</button>";
 		}else{
 			panelHTML += "                        <button class=\"btn btn-default \" type=\"button\" id=\"NewRemember\" type=\"submit\" value=\"Submit\" disabled><em class=\"glyphicon glyphicon-record\"></em>Remember</button>";
 		}
-		
+	
+		panelHTML += "                        <button class=\"btn btn-default \" type=\"button\" id=\"ForgetRememberedDeneWord\" type=\"submit\" value=\"Submit\" disabled><em class=\"glyphicon glyphicon-remove\"></em>Forget</button>";
+	
+
 		panelHTML += "                    </div>";
         panelHTML += "                    <div id=\"UpdateArea\" class=\"TopButtons pull-right col-xs-4\"><div id=\"RefreshCounter\"></div><div id=\"RefreshCounterMessage\">Click to Pause</div></div>";
         panelHTML += "                </div>";
