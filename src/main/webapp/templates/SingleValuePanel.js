acdc5b50398950de32d77d955cb923652003c4e8
Ari Fainchtein <ari@digitalgeppetto.com>
2019-02-25 09:11:38 +1100

    allowing external data in single panel

--- src/main/webapp/templates/SingleValuePanel.js
+++ src/main/webapp/templates/SingleValuePanel.js
@@ -80,7 +80,7 @@ class SingleValuePanel{
                      panelHTML += "<div class=\"col-lg-12 col-md-12 col-sm-12 col-xs-12\">";
                      panelHTML += "<h6>" + nameToDisplay + "</h6>"; 
                      panelHTML +="</div>";// close col lg-4
-                     
+                     panelHTML +="</div>";// close col lg-4
                      
                      
 //                     panelHTML += "<div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-3\">";	
