ad042671b305c4c006fd3378eebfa4c9d2532c05
Ari Fainchtein <ari@digitalgeppetto.com>
2019-03-13 05:05:20 +1100

    fixing the getlogfile processinghanfler

--- src/main/java/com/teleonome/webapp/forms/GetLogFileProcessingHandler.java
+++ src/main/java/com/teleonome/webapp/forms/GetLogFileProcessingHandler.java
@@ -1,10 +1,14 @@
 package com.teleonome.webapp.forms;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
 
 import com.teleonome.framework.exception.ServletProcessingException;
 import com.teleonome.webapp.servlet.ProcessingFormHandler;
@@ -19,7 +23,13 @@ public class GetLogFileProcessingHandler extends ProcessingFormHandler {
 	@Override
 	public void process() throws ServletProcessingException, IOException {
 		// TODO Auto-generated method stub
-
+		String logFileName = request.getParameter("logFileName");
+		String fileInString = FileUtils.readFileToString(new File(logFileName));
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print(fileInString);
+		out.flush();
+		out.close();
 	}
 
 }
