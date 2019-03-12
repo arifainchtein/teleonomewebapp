d8ed49cc8965b74cbb91b34053d6a4e1196f514b
Ari Fainchtein <ari@digitalgeppetto.com>
2019-03-13 07:03:46 +1100

    Adding Analysis to Diagnotics

--- src/main/webapp/templates/DenomeAnalysis.js
+++ src/main/webapp/templates/DenomeAnalysis.js
@@ -5,7 +5,7 @@ class DenomeAnalysis{
 	}
 
 	process(){
-	        var panelHTML = "<iframe id=\"DenomeAnalysisFrame\" src=\"\" width=\"100%\" height=\"300\"></iframe>";
+	        var panelHTML = "<iframe id=\"DenomeAnalysisFrame\" src=\"\" width=\"100%\" height=\"600\"></iframe>";
 	        return panelHTML;
 	}
 }
