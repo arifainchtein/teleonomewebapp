e93d94c97b4950191d85ea6f973376ac89d846d9
Ari Fainchtein <ari@digitalgeppetto.com>
2019-07-04 15:02:17 +1000

    adding Network Sensor

--- src/main/java/com/teleonome/webapp/forms/AddToWhiteListProcessingHandler.java
+++ src/main/java/com/teleonome/webapp/forms/AddToWhiteListProcessingHandler.java
@@ -43,7 +43,7 @@ public class AddToWhiteListProcessingHandler extends ProcessingFormHandler {
 		
 		JSONObject payLoadParentJSONObject = new JSONObject();
 		JSONObject payLoadJSONObject = new JSONObject();
-		payLoadParentJSONObject.put("Mutation Name","Add To White List");
+		payLoadParentJSONObject.put("Mutation Name","Add To WhiteList");
 		payLoadParentJSONObject.put("Payload", payLoadJSONObject);
 		JSONArray updatesArray = new JSONArray();
 		payLoadJSONObject.put("Updates"	, updatesArray);
