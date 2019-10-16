ee5108051749bd9ee754e585b344a5a95c6fadb4
Ari Fainchtein <ari@digitalgeppetto.com>
2019-10-16 16:03:00 +1100

    adding the export view data logic

--- src/main/webapp/SearchFunctions.js
+++ src/main/webapp/SearchFunctions.js
@@ -157,7 +157,7 @@ class SearchFunctions{
 		            success: function (dataString) {
 		            	$('#WaitingWheel').hide();
 		            	//var win = window.open("Data Export", "Title", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=500,top=200,left=200");
-		            	var win = window.open("Data Export", "Title", "status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=500,top=200,left=200");
+		            	var win = window.open("", "Title", "status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=500,top=200,left=200");
 		            	win.document.body.innerHTML = dataString;
 		            },
 		            error: function(data){
