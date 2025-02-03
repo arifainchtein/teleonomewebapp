ce4553d0083c46c7daa3d3bb637963b6380ed33f
Ari Fainchtein <ari@digitalstables.com>
2025-02-03 17:01:38 +1100

    annabelle

--- src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
+++ src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
@@ -37,7 +37,7 @@ import com.teleonome.framework.utils.Utils;
 public class WebAppContextListener implements ServletContextListener {
 	Logger logger ;
 	ServletContext servletContext=null;
-	public final static String BUILD_NUMBER="03/02/2025 16:04";
+	public final static String BUILD_NUMBER="03/02/2025 16:15";
 	private PostgresqlPersistenceManager aDBManager=null;
 	
 	public void contextInitialized(ServletContextEvent sce) {
