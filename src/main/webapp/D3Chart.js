8a06312f6a2738bb44140c6cf7d592d316a58f80
Ari Fainchtein <ari@digitalstables.com>
2025-02-04 13:52:24 +1100

    annabelle

--- src/main/webapp/D3Chart.js
+++ src/main/webapp/D3Chart.js
@@ -1,6 +1,6 @@
 
 function showTelepathonGraph(data) {
-	// Set dimensions
+	// Set dimensions 
 	const margin = {top: 20, right: 30, bottom: 50, left: 50};
 	const width = 800 - margin.left - margin.right;
 	const height = 400 - margin.top - margin.bottom;
