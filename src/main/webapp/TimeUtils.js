8b47d77d229df3e3be72c3aa113305ec74009b6c
Ari Fainchtein <ari@digitalstables.com>
2025-02-06 15:56:34 +1100

    annabell

---
+++ src/main/webapp/TimeUtils.js
@@ -0,0 +1,49 @@
+function analyzeTimeSeriesData(data) {
+    // Sort data by timeString
+    const sortedData = data.sort((a, b) => parseInt(a.timeString) - parseInt(b.timeString));
+    
+    // Calculate average interval
+    let totalInterval = 0;
+    let intervalCount = 0;
+    
+    for (let i = 1; i < sortedData.length; i++) {
+        const interval = parseInt(sortedData[i].timeString) - parseInt(sortedData[i-1].timeString);
+        totalInterval += interval;
+        intervalCount++;
+    }
+    
+    const averageInterval = totalInterval / intervalCount;
+    
+    // Calculate hourly averages
+    const hourlyData = {};
+    // Initialize all hours
+    for (let i = 0; i < 24; i++) {
+        hourlyData[i] = {
+            sum: 0,
+            count: 0,
+            average: 0
+        };
+    }
+    
+    // Group data by hour
+    sortedData.forEach(item => {
+        const date = new Date(parseInt(item.timeString) * 1000);
+        const hour = date.getHours();
+        hourlyData[hour].sum += parseFloat(item.value);
+        hourlyData[hour].count++;
+    });
+    
+    // Calculate averages for each hour
+    for (let hour = 0; hour < 24; hour++) {
+        if (hourlyData[hour].count > 0) {
+            hourlyData[hour].average = hourlyData[hour].sum / hourlyData[hour].count;
+        }
+    }
+    
+    return {
+        averageIntervalSeconds: averageInterval,
+        averageIntervalMinutes: averageInterval / 60,
+        averageIntervalHours: averageInterval / 3600,
+        hourlyAverages: hourlyData
+    };
+}
