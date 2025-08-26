6d8cc4faf853f5fc4283776eaa55b90a90816934
Ari Fainchtein <ari@digitalstables.com>
2025-08-26 12:19:11 +1000

    refining parameters

--- src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
+++ src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
@@ -37,7 +37,7 @@ import com.teleonome.framework.utils.Utils;
 public class WebAppContextListener implements ServletContextListener {
 	Logger logger ;
 	ServletContext servletContext=null;
-	public final static String BUILD_NUMBER="26/08/2025 12:16";
+	public final static String BUILD_NUMBER="26/08/2025 12:19";
 	private PostgresqlPersistenceManager aDBManager=null;
 	
 	public void contextInitialized(ServletContextEvent sce) {
