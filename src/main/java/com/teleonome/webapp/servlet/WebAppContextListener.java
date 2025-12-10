47a0a000ff61f916b3653e34a9bb7098f6687598
Ari Fainchtein <ari@digitalstables.com>
2025-12-10 20:28:15 +1100

    Chinampa Deserializer

--- src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
+++ src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
@@ -37,7 +37,7 @@ import com.teleonome.framework.utils.Utils;
 public class WebAppContextListener implements ServletContextListener {
 	Logger logger ;
 	ServletContext servletContext=null;
-	public final static String BUILD_NUMBER="30/09/2025 12:54";
+	public final static String BUILD_NUMBER="10/12/2025 11:04";
 	private PostgresqlPersistenceManager aDBManager=null;
 	
 	public void contextInitialized(ServletContextEvent sce) {
