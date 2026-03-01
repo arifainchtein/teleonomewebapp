5819abe3dea509f75b5a2c8c59cd055e9167c57b
Ari Fainchtein <ari@digitalstables.com>
2026-03-02 09:32:31 +1100

    new buttons

--- src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
+++ src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
@@ -37,7 +37,7 @@ import com.teleonome.framework.utils.Utils;
 public class WebAppContextListener implements ServletContextListener {
 	Logger logger ;
 	ServletContext servletContext=null;
-	public final static String BUILD_NUMBER="01/03/2026 13:21";
+	public final static String BUILD_NUMBER="02/03/2026 08:11";
 	private PostgresqlPersistenceManager aDBManager=null;
 	
 	public void contextInitialized(ServletContextEvent sce) {
