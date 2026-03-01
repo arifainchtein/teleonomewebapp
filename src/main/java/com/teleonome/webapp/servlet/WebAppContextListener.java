6fc3b97cd2e2d7df6b313f2180009052fe0e2190
Ari Fainchtein <ari@digitalstables.com>
2026-03-01 12:17:09 +1100

    hippocampus

--- src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
+++ src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
@@ -37,7 +37,7 @@ import com.teleonome.framework.utils.Utils;
 public class WebAppContextListener implements ServletContextListener {
 	Logger logger ;
 	ServletContext servletContext=null;
-	public final static String BUILD_NUMBER="28/02/2026 18:04";
+	public final static String BUILD_NUMBER="01/03/2026 11:46";
 	private PostgresqlPersistenceManager aDBManager=null;
 	
 	public void contextInitialized(ServletContextEvent sce) {
