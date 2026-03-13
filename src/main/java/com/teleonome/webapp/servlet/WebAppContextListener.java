f19c517bfa54b4b2cfd0aac2b1e38f1472bdd47b
Ari Fainchtein <ari@digitalstables.com>
2026-03-14 08:14:22 +1100

    adding initial load

--- src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
+++ src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
@@ -37,7 +37,7 @@ import com.teleonome.framework.utils.Utils;
 public class WebAppContextListener implements ServletContextListener {
 	Logger logger ;
 	ServletContext servletContext=null;
-	public final static String BUILD_NUMBER="14/03/2026 07:46";
+	public final static String BUILD_NUMBER="14/03/2026 08:14";
 	private PostgresqlPersistenceManager aDBManager=null;
 	
 	public void contextInitialized(ServletContextEvent sce) {
