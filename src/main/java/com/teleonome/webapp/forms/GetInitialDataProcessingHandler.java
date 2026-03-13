fbfc0e83262f15a33d01f8a5f777d343be610241
Ari Fainchtein <ari@digitalstables.com>
2026-03-14 08:20:01 +1100

    adding initial load

---
+++ src/main/java/com/teleonome/webapp/forms/GetInitialDataProcessingHandler.java
@@ -0,0 +1,41 @@
+package com.teleonome.webapp.forms;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import com.teleonome.framework.exception.ServletProcessingException;
+import com.teleonome.framework.network.NetworkUtilities;
+import com.teleonome.webapp.servlet.ProcessingFormHandler;
+
+public class GetInitialDataProcessingHandler extends ProcessingFormHandler {
+
+	public GetInitialDataProcessingHandler(HttpServletRequest req, HttpServletResponse res,
+			ServletContext servletContext) {
+		super(req, res, servletContext);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void process() throws ServletProcessingException, IOException {
+		// TODO Auto-generated method stub
+		JSONObject interfacesJSONObject = NetworkUtilities.getAvailableAdapters();
+		JSONObject data = new JSONObject();
+		JSONObject denomeJSONObject = (JSONObject) servletContext.getAttribute("CurrentPulse");
+		data.put("interfaces", interfacesJSONObject);
+		data.put("pulse", denomeJSONObject);
+		logger.debug(interfacesJSONObject.toString(4));
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print(data.toString());
+		out.flush();
+		out.close(); 
+
+	}
+
+}
