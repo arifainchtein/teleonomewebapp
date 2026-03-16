a1d1714a3965d4656bea728aa1a245a8a9478daf
Ari Fainchtein <ari@digitalstables.com>
2026-03-17 09:38:32 +1100

    dev

--- src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
+++ src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
@@ -33,30 +33,30 @@ import com.teleonome.framework.utils.Utils;
 
 
 
-   
+
 public class WebAppContextListener implements ServletContextListener {
 	Logger logger ;
 	ServletContext servletContext=null;
-	public final static String BUILD_NUMBER="15/03/2026 19:04";
+	public final static String BUILD_NUMBER="17/03/2026 09:36";
 	private PostgresqlPersistenceManager aDBManager=null;
-	
+
 	public void contextInitialized(ServletContextEvent sce) {
 		String fileName =  Utils.getLocalDirectory() + "lib/Log4J.properties";
 		PropertyConfigurator.configure(fileName);
 		logger = Logger.getLogger(getClass());
-		
+
 		String processName = ManagementFactory.getRuntimeMXBean().getName();
 		logger.warn("WebServerProcess:" + processName +  " BUILD_NUMBER=" + BUILD_NUMBER);
 		System.out.println("WebServerProcess:" + processName +  " BUILD_NUMBER=" + BUILD_NUMBER);
 		try {
 			FileUtils.writeStringToFile(new File("WebServerProcess.info"), processName);
-		//	String webPid = Integer.parseInt(processName.split("@")[0]);
-			
+			//	String webPid = Integer.parseInt(processName.split("@")[0]);
+
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 		// start the thread
 		servletContext = sce.getServletContext();
 		TimeZone timeZone=null;
@@ -69,21 +69,21 @@ public class WebAppContextListener implements ServletContextListener {
 			servletContext.setAttribute("DeneWordsToRemember", deneWordsToRemember);
 			servletContext.setAttribute("TeleonomeName", getTeleonomeName());
 			logger.warn("Refreshing, autocompleteValues");
-        	long now = System.currentTimeMillis();
-        	//JSONObject autoCompleteValues =  getAutoCompleteValues();
-        	//logger.warn("it took " + ((System.currentTimeMillis()-now)/1000)+ " seconds to generate the autocomplete values");
-        	//servletContext.setAttribute("AutoCompleteValues", autoCompleteValues);
-        	
-        	
-			
-			
+			long now = System.currentTimeMillis();
+			//JSONObject autoCompleteValues =  getAutoCompleteValues();
+			//logger.warn("it took " + ((System.currentTimeMillis()-now)/1000)+ " seconds to generate the autocomplete values");
+			//servletContext.setAttribute("AutoCompleteValues", autoCompleteValues);
+
+
+
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			logger.warn(Utils.getStringException(e));
 		}
 		logger.warn("timeZone:" + timeZone );
-		
-		
+
+
 		logger.warn("Teleonome ContextListener Starting PingThread");
 		PingThread aPingThread = new PingThread();
 		aPingThread.start();
@@ -92,124 +92,128 @@ public class WebAppContextListener implements ServletContextListener {
 		aFileWatcherThread.start();
 	}
 
-	
+
 	class PingThread extends Thread{
-	     
-	    public PingThread(){
-	        setDaemon(true);
-	    }
-	    public void run(){
-	        while(true) {
-	        	JSONObject deneWordsToRemember =  getDeneWordsToRemember();
+
+		public PingThread(){
+			setDaemon(true);
+		}
+		public void run(){
+			while(true) {
+				JSONObject deneWordsToRemember =  getDeneWordsToRemember();
 				servletContext.setAttribute("DeneWordsToRemember", deneWordsToRemember);
-				
-	        	logger.debug("Hello from WebServer, executing PingThread refreshing ");
-	        	try {
-	        		double webserverAvailableMemory = Runtime.getRuntime().freeMemory()/1024000;
+
+				logger.debug("Hello from WebServer, executing PingThread refreshing ");
+				try {
+					double webserverAvailableMemory = Runtime.getRuntime().freeMemory()/1024000;
 					double webserverMaxMemory = Runtime.getRuntime().maxMemory()/1024000;
 					JSONObject pingInfo = new JSONObject();
 					pingInfo.put(TeleonomeConstants.WEBSERVER_PROCESS_AVAILABLE_MEMORY, webserverAvailableMemory);
 					pingInfo.put(TeleonomeConstants.WEBSERVER_PROCESS_MAXIMUM_MEMORY, webserverMaxMemory);
 					pingInfo.put(TeleonomeConstants.DATATYPE_TIMESTAMP_MILLISECONDS, System.currentTimeMillis());
-					
-	    			FileUtils.writeStringToFile(new File("WebServerPing.info"), pingInfo.toString());
-	    		//	String webPid = Integer.parseInt(processName.split("@")[0]);
-	    			
-	    		} catch (IOException e1) {
-	    			// TODO Auto-generated catch block
-	    			e1.printStackTrace();
-	    		}
-	        	String teleonomeInString;
-	        	JSONObject denomeJSONObject=null,hippocampusStatusJSONObject=null;
-	        	boolean keepGoing=true;
-	        	int counter=0;
-	        	int max=3;
-	        	do {
-	    			try {
-	    				teleonomeInString = FileUtils.readFileToString(new File("Teleonome.denome"));
-	    				denomeJSONObject = new JSONObject(FileUtils.readFileToString(new File("Teleonome.denome")));
-	    				servletContext.setAttribute("CurrentPulse", denomeJSONObject);
-	    				
-	    				hippocampusStatusJSONObject = new JSONObject(FileUtils.readFileToString(new File("HippocampusStatus.json")));
-	    				servletContext.setAttribute("hippocampusStatusJSONObject", hippocampusStatusJSONObject);
-	    				long now =  System.currentTimeMillis();
-	    				long hippoTime = hippocampusStatusJSONObject.getLong(TeleonomeConstants.DATATYPE_TIMESTAMP_MILLISECONDS);
-	    				boolean hipocampusActive = false;
-	    				if(now>(hippoTime + 60*1000))hipocampusActive = true;
-	    				Identity identity = new Identity(getTeleonomeName(), TeleonomeConstants.NUCLEI_PURPOSE, TeleonomeConstants.DENECHAIN_OPERATIONAL_DATA, TeleonomeConstants.DENE_VITAL,TeleonomeConstants.DENEWORD_TYPE_CURRENT_IDENTITY_MODE);
+
+					FileUtils.writeStringToFile(new File("WebServerPing.info"), pingInfo.toString());
+					//	String webPid = Integer.parseInt(processName.split("@")[0]);
+
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				String teleonomeInString;
+				JSONObject denomeJSONObject=null,hippocampusStatusJSONObject=new JSONObject();
+				boolean keepGoing=true;
+				int counter=0;
+				int max=3;
+				do {
+					try {
+						teleonomeInString = FileUtils.readFileToString(new File("Teleonome.denome"));
+						denomeJSONObject = new JSONObject(FileUtils.readFileToString(new File("Teleonome.denome")));
+						servletContext.setAttribute("CurrentPulse", denomeJSONObject);
+						File hippoFile = new File("HippocampusStatus.json");
+						long now =  System.currentTimeMillis();
+						boolean hipocampusActive = false;
+						if(hippoFile.isFile()) {
+							hippocampusStatusJSONObject = new JSONObject(FileUtils.readFileToString(hippoFile));
+							long hippoTime = hippocampusStatusJSONObject.getLong(TeleonomeConstants.DATATYPE_TIMESTAMP_MILLISECONDS);
+							if(now>(hippoTime + 60*1000))hipocampusActive = true;
+						}else {
+							hipocampusActive=false;
+						}
+						servletContext.setAttribute("hippocampusStatusJSONObject", hippocampusStatusJSONObject);
+						Identity identity = new Identity(getTeleonomeName(), TeleonomeConstants.NUCLEI_PURPOSE, TeleonomeConstants.DENECHAIN_OPERATIONAL_DATA, TeleonomeConstants.DENE_VITAL,TeleonomeConstants.DENEWORD_TYPE_CURRENT_IDENTITY_MODE);
 						String currentIdentityMode = (String) DenomeUtils.getDeneWordByIdentity(denomeJSONObject, identity, TeleonomeConstants.DENEWORD_VALUE_ATTRIBUTE);
 						servletContext.setAttribute("CurrentIdentityMode", currentIdentityMode);
 						servletContext.setAttribute("hipocampusActive", hipocampusActive);
-	    				keepGoing=false;
-	    			} catch (JSONException | IOException  | InvalidDenomeException e) {
-	    				// TODO Auto-generated catch block
-	    				
-	    				logger.warn("line 133 Error reading the denome, waiting one second, counter=" + counter);
-	    				try {
-	    					Thread.sleep(1000);
-	    				} catch (InterruptedException e1) {
-	    					// TODO Auto-generated catch block
-	    					e1.printStackTrace();
-	    				}
-	    				counter++;
-	    				if(counter>max) {
-	    					keepGoing=false;
-	    				}
-	    			} 
-	    		}while(keepGoing);
-	        	counter=0;
-	        	keepGoing=true;
-	        	do {
-	    			try {
-	    				hippocampusStatusJSONObject = new JSONObject(FileUtils.readFileToString(new File("HippocampusStatus.json")));
-	    				servletContext.setAttribute("hippocampusStatusJSONObject", hippocampusStatusJSONObject);
-	    				long now =  System.currentTimeMillis();
-	    				long hippoTime = hippocampusStatusJSONObject.getLong(TeleonomeConstants.DATATYPE_TIMESTAMP_MILLISECONDS);
-	    				boolean hipocampusActive = false;
-	    				if(now<(hippoTime + 60*1000))hipocampusActive = true;
-	    				servletContext.setAttribute("hipocampusActive", hipocampusActive);
-	    				keepGoing=false;
-	    			} catch (JSONException | IOException e) {
-	    				// TODO Auto-generated catch block
-	    				logger.warn("line 173 Error reading the hipocamus status, waiting one second, counter=" + counter);
-	    				try {
-	    					Thread.sleep(1000);
-	    				} catch (InterruptedException e1) {
-	    					// TODO Auto-generated catch block
-	    					e1.printStackTrace();
-	    				}
-	    				counter++;
-	    				if(counter>max) {
-	    					keepGoing=false;
-	    				}
-	    			} 
-	    		}while(keepGoing);
-	        	
+						keepGoing=false;
+					} catch (JSONException | IOException  | InvalidDenomeException e) {
+						// TODO Auto-generated catch block
+
+						logger.warn("line 133 Error reading the denome, waiting one second, counter=" + counter);
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						counter++;
+						if(counter>max) {
+							keepGoing=false;
+						}
+					} 
+				}while(keepGoing);
+				counter=0;
+				keepGoing=true;
+				do {
+					try {
+						hippocampusStatusJSONObject = new JSONObject(FileUtils.readFileToString(new File("HippocampusStatus.json")));
+						servletContext.setAttribute("hippocampusStatusJSONObject", hippocampusStatusJSONObject);
+						long now =  System.currentTimeMillis();
+						long hippoTime = hippocampusStatusJSONObject.getLong(TeleonomeConstants.DATATYPE_TIMESTAMP_MILLISECONDS);
+						boolean hipocampusActive = false;
+						if(now<(hippoTime + 60*1000))hipocampusActive = true;
+						servletContext.setAttribute("hipocampusActive", hipocampusActive);
+						keepGoing=false;
+					} catch (JSONException | IOException e) {
+						// TODO Auto-generated catch block
+						logger.warn("line 173 Error reading the hipocamus status, waiting one second, counter=" + counter);
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						counter++;
+						if(counter>max) {
+							keepGoing=false;
+						}
+					} 
+				}while(keepGoing);
+
 				logger.debug("Refreshing, autocompleteValues");
 				long now = System.currentTimeMillis();
-	        //	JSONObject autoCompleteValues =  getAutoCompleteValues();
-	        	
-	        	logger.debug("it took " + ((System.currentTimeMillis()-now)/1000)+ " seconds to generate the autocomplete values");
-	        	
-		        try {
+				//	JSONObject autoCompleteValues =  getAutoCompleteValues();
+
+				logger.debug("it took " + ((System.currentTimeMillis()-now)/1000)+ " seconds to generate the autocomplete values");
+
+				try {
 					Thread.sleep(1000*10);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-	        }
-	    	
-	    }
-	    
+			}
+
+		}
+
 	}
-	
+
 	public void contextDestroyed(ServletContextEvent sce) {
 		// stop the thread
 	}
 
 	private JSONObject getAutoCompleteValues1() {
 		JSONObject toReturn = new JSONObject();
-		
+
 		JSONArray teleonomeNames = aDBManager.getTeleonomeNamesInOrganism();
 		String teleonomeName, nucleusName, deneChainName, deneName, deneWordName;
 		JSONArray level0 = new JSONArray();
@@ -238,7 +242,7 @@ public class WebAppContextListener implements ServletContextListener {
 				deneChainNames = aDBManager.getDeneChainNamesForTeleonomeInOrganism( teleonomeName, nucleusName);
 				for(int k=0;k<deneChainNames.length();k++) {
 					deneChainName = (String) deneChainNames.get(k);
-					
+
 					if(!level2.has(teleonomeName+SEP+nucleusName)) {
 						ar = new JSONArray();
 					}else {
@@ -246,12 +250,12 @@ public class WebAppContextListener implements ServletContextListener {
 					}
 					ar.put(deneChainName);
 					level2.put(teleonomeName+SEP+nucleusName,ar);
-					
+
 					deneNames = aDBManager.getDeneNamesForTeleonomeInOrganism( teleonomeName, nucleusName, deneChainName);
 					//logger.debug(teleonomeName + SEP + nucleusName+ SEP + deneChainName + " deneNames=" + deneNames.toString(4));
 					for(int l=0;l<deneNames.length();l++) {
 						deneName = (String) deneNames.get(l);
-						
+
 						if(!level3.has(teleonomeName+SEP+nucleusName+SEP+deneChainName)) {
 							ar = new JSONArray();
 						}else {
@@ -281,12 +285,12 @@ public class WebAppContextListener implements ServletContextListener {
 		toReturn.put("Level2", level2);
 		toReturn.put("Level3", level3);
 		toReturn.put("Level4", level4);
-		
+
 		//
-		
+
 		return toReturn;
 	}
-	
+
 	public String getTeleonomeName() {
 		String teleonomeName="";
 		String teleonomeInString="";
@@ -307,7 +311,7 @@ public class WebAppContextListener implements ServletContextListener {
 				keepGoing=false;
 			} catch (JSONException | IOException e) {
 				// TODO Auto-generated catch block
-				
+
 				logger.warn("Error reading the denome, waiting one second, counter=" + counter);
 				try {
 					Thread.sleep(1000);
@@ -321,7 +325,7 @@ public class WebAppContextListener implements ServletContextListener {
 				}
 			}
 		}while(keepGoing);
-		
+
 		return teleonomeName;
 	}
 	/**
@@ -331,13 +335,13 @@ public class WebAppContextListener implements ServletContextListener {
 	 */
 	private JSONObject getDeneWordsToRemember() {
 		JSONObject toReturn = new JSONObject();
-		
+
 		//*************************
 		// first get all the denes of type DENE_TYPE_MNEMOSYCON_DENEWORDS_TO_REMEMBER 
 		//
 		// Hashtable<String,ArrayList> deneWordsToRememberByTeleonome
-		
-		
+
+
 		try {
 			JSONObject denomeJSONObject = new JSONObject(FileUtils.readFileToString(new File("Teleonome.denome")));
 			JSONObject dataForBrowser = new JSONObject();
@@ -345,7 +349,7 @@ public class WebAppContextListener implements ServletContextListener {
 			String timeZoneId = "UTC";
 			int basePulseFrequency=60;
 			JSONObject anMnemosyconsDeneChainJSONObject;
-			
+
 			anMnemosyconsDeneChainJSONObject = DenomeUtils.getDeneChainByName(denomeJSONObject,TeleonomeConstants.NUCLEI_INTERNAL,  TeleonomeConstants.DENECHAIN_MNEMOSYCONS);
 			Hashtable deneWordsToRememberByTeleonome = new Hashtable();
 			JSONObject rememberedWordsMnemosyconJSONObject;
@@ -357,12 +361,12 @@ public class WebAppContextListener implements ServletContextListener {
 			JSONArray mnemosyconDenesJSONArray;
 			//logger.debug("in denomemagager anMnemosyconsDeneChainJSONObject= " + anMnemosyconsDeneChainJSONObject);
 			if(anMnemosyconsDeneChainJSONObject!=null){
-				
+
 				JSONArray denes = anMnemosyconsDeneChainJSONObject.getJSONArray("Denes");
 				JSONObject rememberedDeneWordJSONObject, clonedRememberedDeneWordJSONObject;
-				
+
 				//String rememberedDeneWordUnit;
-				
+
 				for(int i=0;i<denes.length();i++){
 					if(denes.getJSONObject(i).has(TeleonomeConstants.DENE_DENE_TYPE_ATTRIBUTE) && denes.getJSONObject(i).getString(TeleonomeConstants.DENE_DENE_TYPE_ATTRIBUTE).equals(TeleonomeConstants.DENE_TYPE_MNEMOSYCON_DENEWORDS_TO_REMEMBER)){
 						rememberedWordsMnemosyconJSONObject = denes.getJSONObject(i);
@@ -380,14 +384,14 @@ public class WebAppContextListener implements ServletContextListener {
 								//
 								rememberedDeneWordSourceDeneWordPointer = (new Identity(getTeleonomeName(), TeleonomeConstants.NUCLEI_INTERNAL,  TeleonomeConstants.DENECHAIN_MNEMOSYCONS, rememberedWordsMnemosyconJSONObject.getString(TeleonomeConstants.DENE_DENE_NAME_ATTRIBUTE),rememberedDeneWordJSONObject.getString(TeleonomeConstants.DENEWORD_NAME_ATTRIBUTE))).toString();
 								logger.debug( " rememberedDeneWordPointer= " + rememberedDeneWordPointer + " rememberedDeneWordSourceDeneWordPointer=" + rememberedDeneWordSourceDeneWordPointer );
-								
+
 								clonedRememberedDeneWordJSONObject = new JSONObject(rememberedDeneWordJSONObject, JSONObject.getNames(rememberedDeneWordJSONObject));
 								clonedRememberedDeneWordJSONObject.put(TeleonomeConstants.DENEWORD_IDENTITY_ATTRIBUTE ,rememberedDeneWordSourceDeneWordPointer);
 								logger.debug("Storing a rememberedword as key=" + rememberedDeneWordPointer + " clonedRememberedDeneWordJSONObject=" + clonedRememberedDeneWordJSONObject.toString(4));
 								toReturn.put(rememberedDeneWordPointer, clonedRememberedDeneWordJSONObject);
 							}
 						}
-			
+
 					}
 				}
 				toReturn = getSorted(toReturn);
@@ -404,8 +408,8 @@ public class WebAppContextListener implements ServletContextListener {
 		}
 		return toReturn;
 	}
-		
-		
+
+
 	private  JSONObject getSorted(final JSONObject obj) {
 		final JSONObject json = new JSONObject();
 		try {
@@ -423,10 +427,10 @@ public class WebAppContextListener implements ServletContextListener {
 
 		return json;
 	}
-	 
 
-	
-	
+
+
+
 	public JSONArray getAllDeneWordAttributeByDeneWordTypeFromDene(JSONObject deneJSONObject , String type, String whatToBring) throws JSONException{
 		JSONArray deneWords = deneJSONObject.getJSONArray("DeneWords");
 		JSONArray toReturn = new JSONArray();
@@ -444,11 +448,11 @@ public class WebAppContextListener implements ServletContextListener {
 		}
 		return toReturn;
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 	private TimeZone getTimeZone() throws IOException{
 
 		JSONObject denomeJSONObject = new JSONObject(FileUtils.readFileToString(new File("Teleonome.denome")));
@@ -476,7 +480,7 @@ public class WebAppContextListener implements ServletContextListener {
 		}
 		return currentTimeZone;
 	}
-	
+
 
 
 }
