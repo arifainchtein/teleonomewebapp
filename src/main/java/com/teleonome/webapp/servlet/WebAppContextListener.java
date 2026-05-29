3642377ae35ec278d8e399855c763c25699168a4
Ari Fainchtein <ari@digitalstables.com>
2026-05-29 12:23:25 +1000

    asyncdata

--- src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
+++ src/main/java/com/teleonome/webapp/servlet/WebAppContextListener.java
@@ -37,7 +37,7 @@ import com.teleonome.framework.utils.Utils;
 public class WebAppContextListener implements ServletContextListener {
 	Logger logger ;
 	ServletContext servletContext=null;
-	public final static String BUILD_NUMBER="27/05/2026 13:01";
+	public final static String BUILD_NUMBER="29/05/2026 12:22";
 	private PostgresqlPersistenceManager aDBManager=null;
 
 	public void contextInitialized(ServletContextEvent sce) {
--- src/main/webapp/RenderingEngine.js
+++ src/main/webapp/RenderingEngine.js
@@ -761,7 +761,7 @@ function updateTelepathonsView(text){
 	var telepathon = JSON.parse(text);
 	
 	var telepathonName = telepathon["Name"];
-	if(telepathonName!="Top Tank" &&  telepathonName!="Chinampa" &&  telepathonName!="SeedlingMonitor" ){
+	if(telepathonName!="TopTank" &&  telepathonName!="Chinampa" &&  telepathonName!="SeedlingMonitor" ){
 		return ;
 	} 
 	var currentFunctionValue;	
@@ -1090,7 +1090,7 @@ function refreshTelepathonsView(){
 		var currentFunctionTitle;
 			for(var j13=0;j13<deneChains.length;j13++){
 				telepathonName = deneChains[j13]["Name"];
-				if(telepathonName!="Top Tank" &&  telepathonName!="Chinampa" &&  telepathonName!="SeedlingMonitor" ){
+				if(telepathonName!="TopTank" &&  telepathonName!="Chinampa" &&  telepathonName!="SeedlingMonitor" ){
 					continue ;
 				} 
 				 serialnumber = deneChains[j13]["Serial Number"];
